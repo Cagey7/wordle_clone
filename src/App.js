20d9db0c346150a78527e94c53b6586e311779f9
Cagey7 <dagodecagey@gmail.com>
2025-01-23 23:42:46 +0500

    Added letter input

--- src/App.js
+++ src/App.js
@@ -1,75 +1,99 @@
 import { useState } from 'react';
 import './App.css';
 
-class Letter {
-  constructor(name, status) {
-    this.name = name;
-    this.status = status;
+
+function getStatusClass(letter) {
+  let statusClass = '';
+  if (letter.status === "correct") {
+    statusClass = "correct";
+  } else if (letter.status === "incorrect") {
+    statusClass = "incorrect";
+  } else if (letter.status === "exist") {
+    statusClass = "exist";
   }
+  return statusClass;
 }
 
-
-class Word {
-  constructor(letters) {
-    this.letters = letters;
-  }
-}
-
-
-class GameField {
-  constructor(words) {
-    this.words = words;
-  }
-}
-
-const words = new Array(6).fill(null).map(() => {
-  const letters = new Array(5).fill(null).map(() => new Letter("a", "correct"));
-  return new Word(letters);
-});
-
-
-const initialGameField = new GameField(words)
 
 const keyBoardLetters = [["й","ц","у","к","е","н","г","ш","щ","з","х","ъ"],
                   ["ф","ы","в","а","п","р","о","л","д","ж","э"],
                   ["<","я","ч","с","м","и","т","ь","б","ю","Enter"]]
 
-const initialKeyboard = []
-for (const row of keyBoardLetters){
-  const newRow = []
-  for (const letter of row) {
-    newRow.push(new Letter(letter, ""))
-  }
-  initialKeyboard.push(newRow)
-}
+
+let initGameField = Array.from({ length: 5 }, () =>
+  Array.from({ length: 6 }, () => ({ name: "", status: "" }))
+);
 
 
-console.log(initialGameField)
+const initialKeyboard = keyBoardLetters.map(row => 
+  row.map(letter => ({ name: letter, status: "" }))
+);
+
 
 function App() {
-  const [gameField, setgameField] = useState(initialGameField);
+  const [gameField, setGameField] = useState(initGameField);
   const [keyboard, setKeyboard] = useState(initialKeyboard);
+
+  function handleKeyboardClick(clickedLetter) {
+    let changed = false;
+
+    for (const word of gameField) {
+      if (word.at(-2).name !== "") {
+        return
+      }
+    }
+
+    const newGameField = gameField.map((word) => (
+      word.map((letter) => {
+        if (!changed && letter.name === "") {
+          changed = true;
+          return { name: clickedLetter.name, status: "" };
+        }
+        return letter;
+      })
+    ))
+
+    setGameField(newGameField);
+  }
+
+  function deleteLastLetter() {
+    return
+  }
 
   return (
     <>
       <div className="words-field">
-        {gameField.words.map((word, wordIndex) => (
-          word.letters.map((letter, letterIndex) => {
-            
-            return <div className={`cell ${letter.status === "correct" ? "correct" : ""} ${letter.status === "incorrect" ? "incorrect" : ""} ${letter.status === "exist" ? "exist" : ""} `} key={letterIndex}>{letter.name}</div>;
+        {gameField.map((word, wordIndex) => (
+          word.map((letter, letterIndex) => {
+            return <div className={`cell ${getStatusClass(letter)} `} 
+                        key={letterIndex}>{letter.name}</div>;
           })
         ))}
       </div>
 
       {keyboard.map((row, rowKey) => (
         <div className="keyboard" key={rowKey}>
-          {row.map((letter, letterKey) => (
-            letter.name === "enter" || letter.name === "<" ? (
-              <div className="key key-wide" key={letterKey}>{letter.name}</div>
-            ) : (
-              <div className="key" key={letterKey}>{letter.name}</div>
-            )
-          ))}
+          {row.map((letter, letterKey) => {
+            if (letter.name === "enter") {
+              return (
+                <div className={`key key-wide ${getStatusClass(letter)}`} key={letterKey}>
+                  {letter.name}
+                </div>
+              );
+            } else if (letter.name === "<") {
+              return (
+                <div className={`key key-wide ${getStatusClass(letter)}`} key={letterKey} onClick={() => deleteLastLetter()}>
+                  {letter.name}
+                </div>
+              );
+            } else {
+              return (
+                <div className={`key ${getStatusClass(letter)}`} key={letterKey} onClick={() => handleKeyboardClick(letter)}>
+                  {letter.name}
+                </div>
+              );
+            }
+          })}
         </div>
       ))}
 
