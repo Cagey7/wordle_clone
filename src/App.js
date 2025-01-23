8eb47a458052d98752e51b23097d3c2169b12074
Cagey7 <96425032+Cagey7@users.noreply.github.com>
2025-01-23 17:08:09 +0500

    Created main game fields

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+class Letter {
+  constructor(name, status) {
+    this.name = name;
+    this.status = status;
+  }
+}
+
+
+class Word {
+  constructor(letters) {
+    this.letters = letters;
+  }
+}
+
+
+class GameField {
+  constructor(words) {
+    this.words = words;
+  }
+}
+
+const words = new Array(6).fill(null).map(() => {
+  const letters = new Array(5).fill(null).map(() => new Letter("a", "correct"));
+  return new Word(letters);
+});
+
+
+const initialGameField = new GameField(words)
+
+const keyBoardLetters = [["й","ц","у","к","е","н","г","ш","щ","з","х","ъ"],
+                  ["ф","ы","в","а","п","р","о","л","д","ж","э"],
+                  ["<","я","ч","с","м","и","т","ь","б","ю","Enter"]]
+
+const initialKeyboard = []
+for (const row of keyBoardLetters){
+  const newRow = []
+  for (const letter of row) {
+    newRow.push(new Letter(letter, ""))
+  }
+  initialKeyboard.push(newRow)
+}
+
+
+console.log(initialGameField)
+
 function App() {
+  const [gameField, setgameField] = useState(initialGameField);
+  const [keyboard, setKeyboard] = useState(initialKeyboard);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="words-field">
+        {gameField.words.map((word, wordIndex) => (
+          word.letters.map((letter, letterIndex) => {
+            
+            return <div className={`cell ${letter.status === "correct" ? "correct" : ""} ${letter.status === "incorrect" ? "incorrect" : ""} ${letter.status === "exist" ? "exist" : ""} `} key={letterIndex}>{letter.name}</div>;
+          })
+        ))}
+      </div>
+
+      {keyboard.map((row, rowKey) => (
+        <div className="keyboard" key={rowKey}>
+          {row.map((letter, letterKey) => (
+            letter.name === "enter" || letter.name === "<" ? (
+              <div className="key key-wide" key={letterKey}>{letter.name}</div>
+            ) : (
+              <div className="key" key={letterKey}>{letter.name}</div>
+            )
+          ))}
+        </div>
+      ))}
+
+    </>
+
   );
 }
 
